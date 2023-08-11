8eedcfb7bf807ae95265538d0dc4e373129d3d45
Andrew Miotke <andrew@andrewmiotke.com>
2023-08-11 16:07:05 -0700

    TIL: AppStorage
    - Adding Xcode project file

---
+++ code_examples/Appstorage/Appstorage/AppstorageApp.swift
@@ -0,0 +1,18 @@
+//
+//  AppstorageApp.swift
+//  Appstorage
+//
+//  Created by Andrew on 8/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AppstorageApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
